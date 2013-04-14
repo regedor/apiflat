c520e9c5e86568cf6265af362f46dedcd60d0396
Miguel Fernandes <wecklness@gmail.com>
2013-04-14 13:02:21 +0100

    Create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apiflat::Application.config.secret_token = '087653df824eba6ed1bec88f4fc4cf47e380df3116c43328971086e9be185fa3f917c259375597f01171fa0199a0a24c20bd1bb34c75827994142a9d8c3f36d0'
