b53a8c49f5bac6adaf61c4102d12d56e640a3e6f
Miguel Fernandes <wecklness@gmail.com>
2013-05-12 19:45:51 +0100

    Create Home model and add it to ActiveAdmin resources

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512182128) do
+ActiveRecord::Schema.define(:version => 20130512184121) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -45,5 +45,14 @@ ActiveRecord::Schema.define(:version => 20130512182128) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "homes", :force => true do |t|
+    t.string   "street"
+    t.string   "number"
+    t.string   "post_code"
+    t.string   "city"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
