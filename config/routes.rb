b2e13db534767c4a765d6d15b2ceb4d1f340735c
Miguel Fernandes <wecklness@gmail.com>
2013-05-12 19:33:42 +0100

    Set root to ActiveAdmin login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Apiflat::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root :to => "admin/dashboard#index"
+  
   ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
