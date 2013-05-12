25e11584056b46734c0d73536672bff6dcd2008e
Miguel Fernandes <wecklness@gmail.com>
2013-05-12 18:45:20 +0100

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apiflat::Application.config.secret_token = '920d37fae0b6744d8aabb7991a90c01363a76ace89d0c877e42218eccce430ee82e388a83a9478693450b0923019868de8c0974b660f3ee5612b246288afba55'
