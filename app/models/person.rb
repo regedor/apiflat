f9a5ba37446505b11c46081d062632dbcce0e4be
Miguel Fernandes <wecklness@gmail.com>
2013-05-14 21:25:14 +0100

    Fix model relationships

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,4 @@
 class Person < ActiveRecord::Base
   attr_accessible :admin_user_id, :name
+  has_one :admin_user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130513201040) do
+ActiveRecord::Schema.define(:version => 20130514201059) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -50,8 +50,9 @@ ActiveRecord::Schema.define(:version => 20130513201040) do
     t.date     "done_at"
     t.integer  "value"
     t.integer  "person_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.integer  "duty_type_id"
   end
 
   create_table "duty_types", :force => true do |t|
