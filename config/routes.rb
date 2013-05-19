948a2195cee2f35aa0ff44e7c66107a9000c98eb
Miguel Fernandes <wecklness@gmail.com>
2013-05-19 13:51:19 +0100

    Set all models under the Home resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,17 @@ Apiflat::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   root :to => "admin/dashboard#index"
-  
+
+  resources :homes do
+    resources :duties
+    resources :duty_types
+    resources :people
+    resources :purchases
+    resources :purchase_types
+    resources :tasks
+    resources :task_types
+  end
+
   ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
